5bcbe6cb5e116610909af6aa3c39592776fc85ea
BilkisMelly <146930277+BilkisMelly@users.noreply.github.com>
2023-10-04 09:40:11 +0100

    Add files via upload

---
+++ store.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("mySideNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.1)"
+}
+
+function closeNav() {
+    document.getElementById("mySideNav").style.width ="0";
+    document.getElementById("main").style.marginLeft ="0";
+    document.body.style.backgroundColor = "white";
+}
+
+//this is for slideshow: line 15-33
